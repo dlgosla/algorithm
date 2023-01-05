20ea577647578b72b0b6884bb20046b39f780322
dlgosla <dlgoslaek1@gmail.com>
2023-01-05 20:04:32 +0900

    폴더 변경

---
+++ 2023-01/1주차/게으른 백곰.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+
+n, k = map(int, input().split())
+
+buckets = defaultdict(int)
+end = 0
+for i in range(n):
+    g, x = map(int, input().split())
+    buckets[x] = g
+    if end < x:
+        end = x
+
+curr_sum = 0
+for i in range(k):
+    curr_sum += buckets[i]
+
+# print(curr_sum)
+max_sum = 0
+for i in range(end):
+    left = i - k - 1
+    right = i + k
+
+    curr_sum = curr_sum + buckets[right] - buckets[left]
+    if curr_sum > max_sum:
+        max_sum = curr_sum
+
+# print()
+print(max_sum)
