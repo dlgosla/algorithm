7299b9e85640d6e70dcfe3d3dff16e77057f4a89
dlgosla <dlgoslaek1@gmail.com>
2023-04-12 00:19:14 +0900

    [PGS] 디펜스 게임 / 1시간30분 힌트 참고

---
+++ 2023-04/디펜스게임.py
@@ -0,0 +1,40 @@
+"""
+무적권은 지나온 라운드에서 가장 많은 적이 나왔던 라운드에서 사용해야 매우 효과적이다.
+무적권을 사용하면, 병사의 수가 차감되지 않는다.
+
+"""
+
+
+from heapq import heappop, heappush, heappushpop
+
+
+def solution(n, k, enemy):
+    answer = 0
+    hq = []
+
+    for curr_enemy in enemy:
+
+        # 병사가 현재 적군 수 보다 적다면
+        if n < curr_enemy:
+            # 무적권 사용할 수 있으면
+            if k > 0:
+                max_num = -heappushpop(hq, -curr_enemy)
+                n += max_num
+                n -= curr_enemy
+                k -= 1
+
+            # 못하면 끝
+            else:
+                break
+        else:
+            heappush(hq, -curr_enemy)
+            n -= curr_enemy
+
+        answer += 1
+
+    return answer
+
+
+# # solution(7, 3, [4, 2, 4, 5, 3, 3, 1])
+# # print(solution(2, 2, [7, 1, 2]))
+# # solution(2, 4, [3, 3, 3, 3])
